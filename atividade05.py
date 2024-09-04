059f71845a697eafe9b665a194defd2ae94b2222
henzoxz <henzomag2009@gmail.com>
2024-09-04 16:08:20 -0300

    minuto_para_segundo

---
+++ atividade05.py
@@ -0,0 +1,6 @@
+# Escreva um programa que receba um valor em minutos e converta para segundos.
+minuto = int(input(" Digite o valor em Minuto "))
+segundo = 60
+resultado = minuto*segundo 
+
+print (resultado)
